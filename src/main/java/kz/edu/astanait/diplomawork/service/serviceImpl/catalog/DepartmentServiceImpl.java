7b72eaf7aad40be9a5a692c06e8820cfee98a60b
ybolat <erabeast@mail.ru>
2022-03-15 09:52:09 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/service/serviceImpl/catalog/DepartmentServiceImpl.java
+++ src/main/java/kz/edu/astanait/diplomawork/service/serviceImpl/catalog/DepartmentServiceImpl.java
@@ -8,6 +8,7 @@ import kz.edu.astanait.diplomawork.service.serviceInterface.catalog.DepartmentSe
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,11 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Autowired
     public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public List<Department> getAll() {
+        return this.departmentRepository.findAll();
     }
 
     @Override
--- src/main/java/kz/edu/astanait/diplomawork/service/serviceInterface/catalog/DepartmentService.java
+++ src/main/java/kz/edu/astanait/diplomawork/service/serviceInterface/catalog/DepartmentService.java
@@ -2,9 +2,12 @@ package kz.edu.astanait.diplomawork.service.serviceInterface.catalog;
 
 import kz.edu.astanait.diplomawork.model.catalog.Department;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentService {
+
+    List<Department> getAll();
 
     Optional<Department> getById(Long id);
 
