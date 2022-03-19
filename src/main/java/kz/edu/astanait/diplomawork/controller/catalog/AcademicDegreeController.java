31954b0b9da4e3f96f2c185d134c99bdf500f1d6
AlikhanZhomartov <alikhansoftwareengineer@gmail.com>
2022-03-19 14:02:54 +0600

    preAuthorize

--- src/main/java/kz/edu/astanait/diplomawork/controller/catalog/AcademicDegreeController.java
+++ src/main/java/kz/edu/astanait/diplomawork/controller/catalog/AcademicDegreeController.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/catalog/academic-degree")
+@PreAuthorize("hasRole('ROLE_CHALLENGER')")
 public class AcademicDegreeController {
 
     private final AcademicDegreeService academicDegreeService;
