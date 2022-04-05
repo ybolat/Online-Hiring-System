963c0f3d8a931d5b5262eb50a437df09f37cfda8
Aibyn002 <sagyndyk.aibyn@gmail.com>
2022-04-05 12:11:39 +0600

    Test_Controller

--- src/test/java/kz/edu/astanait/diplomawork/controller/user/CommissionControllerTest.java
+++ src/test/java/kz/edu/astanait/diplomawork/controller/user/CommissionControllerTest.java
@@ -1,13 +1,21 @@
 package kz.edu.astanait.diplomawork.controller.user;
 
+import kz.edu.astanait.diplomawork.service.serviceInterface.user.CommissionService;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommissionControllerTest {
 
+    private final CommissionService commissionService;
+
+    CommissionControllerTest(CommissionService commissionService) {
+        this.commissionService = commissionService;
+    }
+
     @Test
     void getById() {
+        Long id = 1L;
+        assertFalse(this.commissionService.getById(id).isEmpty());
     }
 
     @Test
