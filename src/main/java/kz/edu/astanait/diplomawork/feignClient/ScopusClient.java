20e8ef1fc836be9583f64832b1b9901935094023
ybolat <erabeast@mail.ru>
2022-04-12 13:02:06 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/feignClient/ScopusClient.java
+++ src/main/java/kz/edu/astanait/diplomawork/feignClient/ScopusClient.java
@@ -2,7 +2,7 @@ package kz.edu.astanait.diplomawork.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "https://api.elsevier.com/content/")
+@FeignClient(name = "https://api.elsevier.com/")
 public interface ScopusClient {
 
 }
