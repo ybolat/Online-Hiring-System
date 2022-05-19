92041c81e84bcf2bb503c12505322a652d626d12
ybolat <erabeast@mail.ru>
2022-05-19 18:54:43 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/controller/security/PinCodeController.java
+++ src/main/java/kz/edu/astanait/diplomawork/controller/security/PinCodeController.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.PermitAll;
+
 @RestController
 @RequestMapping("/api/v1/pin-code")
+@PermitAll
 public class PinCodeController extends ExceptionHandling {
 
     private final PinCodeService pinCodeService;
