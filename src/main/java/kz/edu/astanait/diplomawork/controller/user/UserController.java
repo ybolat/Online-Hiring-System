5fafc2faaf80d1b253220fe5f059203ddb3d8c6e
ybolat <erabeast@mail.ru>
2022-05-19 19:29:08 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/controller/user/UserController.java
+++ src/main/java/kz/edu/astanait/diplomawork/controller/user/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -58,6 +59,7 @@ public class UserController extends ExceptionHandling {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/forgot-password")
     public ResponseEntity<HttpStatus> changePassword(@RequestParam(name = "email") String email) throws MessagingException {
         this.userService.forgotPassword(email);
