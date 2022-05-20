168887b1847d434c11732184bf6cc73a6808f043
ybolat <erabeast@mail.ru>
2022-05-20 11:06:56 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/repository/catalog/PublicationsTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.edu.astanait.diplomawork.repository.catalog;
-
-import kz.edu.astanait.diplomawork.model.catalog.PublicationsType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublicationsTypeRepository extends JpaRepository<PublicationsType, Long> {
-}
