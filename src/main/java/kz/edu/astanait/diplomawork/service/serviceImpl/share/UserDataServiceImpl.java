0aa886616fa6a5aefac414051cdb66e78a0b498a
ybolat <erabeast@mail.ru>
2022-05-20 12:07:45 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/service/serviceImpl/share/UserDataServiceImpl.java
+++ src/main/java/kz/edu/astanait/diplomawork/service/serviceImpl/share/UserDataServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -81,26 +82,26 @@ public class UserDataServiceImpl implements UserDataService {
             userDataDtoResponse.setPhoto(DocumentMapper.documentsToDto(userDocument.getPhoto()));
             userDataDtoResponse.setPositionDtoResponse(PositionMapper.positionToDto(userProfessionalInfo.getVacancy().getPosition()));
             userDataDtoResponse.setAcademicDegreeDtoResponse(AcademicDegreeMapper.academicDegreeToDto(userProfessionalInfo.getAcademicDegree()));
-            userDataDtoResponse.setAcademicTitleDtoResponse(AcademicTitleMapper.academicTitleToDto(userProfessionalInfo.getAcademicTitle()));
-            userDataDtoResponse.setScopusId(userProfessionalInfo.getScopus());
-            userDataDtoResponse.setScopusHIndex(userProfessionalInfo.getScopusHIndex());
-            userDataDtoResponse.setScopusLink(userProfessionalInfo.getScopusLink());
-            userDataDtoResponse.setResearchId(userProfessionalInfo.getResearch());
-            userDataDtoResponse.setResearchLink(userProfessionalInfo.getResearchLink());
-            userDataDtoResponse.setResearchHIndex(userProfessionalInfo.getResearchHIndex());
-            userDataDtoResponse.setGoogleScholarLing(userProfessionalInfo.getGoogleScholar());
-            userDataDtoResponse.setGoogleScholarHIndex(userProfessionalInfo.getGoogleScholarHIndex());
+            if (Objects.nonNull(userProfessionalInfo.getAcademicTitle())) userDataDtoResponse.setAcademicTitleDtoResponse(AcademicTitleMapper.academicTitleToDto(userProfessionalInfo.getAcademicTitle()));
+            if (Strings.isNotBlank(userProfessionalInfo.getScopus())) userDataDtoResponse.setScopusId(userProfessionalInfo.getScopus());
+            if (Objects.nonNull(userProfessionalInfo.getScopusHIndex())) userDataDtoResponse.setScopusHIndex(userProfessionalInfo.getScopusHIndex());
+            if (Strings.isNotBlank(userProfessionalInfo.getScopusLink())) userDataDtoResponse.setScopusLink(userProfessionalInfo.getScopusLink());
+            if (Strings.isNotBlank(userProfessionalInfo.getResearch())) userDataDtoResponse.setResearchId(userProfessionalInfo.getResearch());
+            if (Strings.isNotBlank(userProfessionalInfo.getResearchLink())) userDataDtoResponse.setResearchLink(userProfessionalInfo.getResearchLink());
+            if (Objects.nonNull(userProfessionalInfo.getResearchHIndex())) userDataDtoResponse.setResearchHIndex(userProfessionalInfo.getResearchHIndex());
+            if (Strings.isNotBlank(userProfessionalInfo.getGoogleScholar())) userDataDtoResponse.setGoogleScholarLing(userProfessionalInfo.getGoogleScholar());
+            if (Objects.nonNull(userProfessionalInfo.getGoogleScholarHIndex())) userDataDtoResponse.setGoogleScholarHIndex(userProfessionalInfo.getGoogleScholarHIndex());
             userDataDtoResponse.setOrcidLink(userProfessionalInfo.getOrcid());
             userDataDtoResponse.setExperience(userProfessionalInfo.getExperience());
             userDataDtoResponse.setSubjectDtoResponseList(subjectList.stream().map(SubjectMapper::subjectToDto).collect(Collectors.toList()));
             userDataDtoResponse.setScientificInterests(userProfessionalInfo.getScientificInterests());
-            userDataDtoResponse.setProjectDtoResponse(projectList.stream().map(ProjectMapper::projectToDto).collect(Collectors.toList()));
-            userDataDtoResponse.setArticleDtoResponses(articleList.stream().map(ArticleMapper::articleToDto).collect(Collectors.toList()));
-            userDataDtoResponse.setDevelopmentDtoResponse(developmentList.stream().map(DevelopmentMapper::developmentToDto).collect(Collectors.toList()));
-            userDataDtoResponse.setIntelligenceLegalDocumentDtoResponse(intelligenceLegalDocuments.stream().map(IntelligenceLegalDocumentMapper::intelligenceLegalDocumentToDto).collect(Collectors.toList()));
+            if (Objects.nonNull(projectList)) userDataDtoResponse.setProjectDtoResponse(projectList.stream().map(ProjectMapper::projectToDto).collect(Collectors.toList()));
+            if (Objects.nonNull(articleList)) userDataDtoResponse.setArticleDtoResponses(articleList.stream().map(ArticleMapper::articleToDto).collect(Collectors.toList()));
+            if (Objects.nonNull(developmentList)) userDataDtoResponse.setDevelopmentDtoResponse(developmentList.stream().map(DevelopmentMapper::developmentToDto).collect(Collectors.toList()));
+            if (Objects.nonNull(intelligenceLegalDocuments)) userDataDtoResponse.setIntelligenceLegalDocumentDtoResponse(intelligenceLegalDocuments.stream().map(IntelligenceLegalDocumentMapper::intelligenceLegalDocumentToDto).collect(Collectors.toList()));
             userDataDtoResponse.setEmail(user.getEmail());
             userDataDtoResponse.setPhone(user.getPhone());
-            userDataDtoResponse.setCertificateDtoResponseList(certificates.stream().map(CertificateMapper::certificateToDto).collect(Collectors.toList()));
+            if (Objects.nonNull(certificates)) userDataDtoResponse.setCertificateDtoResponseList(certificates.stream().map(CertificateMapper::certificateToDto).collect(Collectors.toList()));
             userDataDtoResponse.setCv(DocumentMapper.documentsToDto(userDocument.getCv()));
         }
         return userDataDtoResponse;
