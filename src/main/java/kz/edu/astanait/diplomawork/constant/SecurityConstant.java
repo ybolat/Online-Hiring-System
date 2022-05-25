3a798239f4b96b7267998f45174e3d9642ebad74
ybolat <erabeast@mail.ru>
2022-05-25 18:29:22 +0600

    commit

--- src/main/java/kz/edu/astanait/diplomawork/constant/SecurityConstant.java
+++ src/main/java/kz/edu/astanait/diplomawork/constant/SecurityConstant.java
@@ -2,5 +2,14 @@ package kz.edu.astanait.diplomawork.constant;
 
 public class SecurityConstant {
 
-    public static final String [] PERMITTED_URL = {"/api/v1/user/authorization/**", "/api/v1/user/registration/**", "/api/v1/user/activate**", "/api/v1/pin-code/create**", "/api/v1/user/forgot-password**", "/api/v1/hiring/vacancy/get-all", "/swagger-ui/**", "/api-docs", "/api-docs.yaml", "/swagger-ui.html"};
+    public static final String [] PERMITTED_URL = {"/api/v1/user/authorization/**",
+            "/api/v1/user/registration/**",
+            "/api/v1/user/activate**",
+            "/api/v1/pin-code/create**",
+            "/api/v1/user/forgot-password**",
+            "/api/v1/hiring/vacancy/get-all",
+            "/swagger-ui/**",
+            "/api-docs",
+            "/api-docs.yaml",
+            "/swagger-ui.html"};
 }
